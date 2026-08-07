038b9fc884831e9169b141db3c7a0f04735e6802
abhaychauhanmarkup-spec <abhay.chauhan.markup@gmail.com>
2026-08-07 11:50:39 +0530

    cs ADD

--- src/Screens/Landing/Landing.tsx
+++ src/Screens/Landing/Landing.tsx
@@ -1,17 +1,10 @@
 // app/page.js (no "use client" needed)
-import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
-import Hero from "../../components/Hero/Hero";
-import BrandShowcase from "../../components/BrandShowcase/BrandShowcase";
-import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
-import Banner from "../../components/Banner/Banner";
-import LoginScreen from "../Auth/LoginScreen";
+import Header from "../../components/Header";
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
-      
-   <Hero/> 
-    
+      <Header />
     </div>
   );
 }
