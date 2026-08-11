6e96dc4f6654461490754b0c560796bdf18470c9
Sudhir Gupta <sudhir.gupta@markupmail.com>
2026-08-11 20:27:54 +0530

    Change

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,4 +1,10 @@
+import { Suspense } from 'react';
 import Product from "../../Screens/Inner/product/page";
+
 export default function Page() {
-  return <Product />;
+  return (
+    <Suspense fallback={<div>Loading products...</div>}>
+      <Product />
+    </Suspense>
+  );
 }
